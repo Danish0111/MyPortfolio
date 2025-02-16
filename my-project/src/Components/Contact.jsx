962c101b3af51c226c56815ea4646a1a2d819684
Danish <164894540+Danish0111@users.noreply.github.com>
2025-02-16 20:16:17 +0530

    Update Contact.jsx

--- my-project/src/Components/Contact.jsx
+++ my-project/src/Components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
             message: data.message
         }
         try{
-            const response = await fetch("https://portfolio-api-neon-alpha.vercel.app", {
+            const response = await fetch("https://portfolio-api-dun-ten.vercel.app", {
                 method: "POST", 
                 headers: {
                     "Content-Type": "application/json"
