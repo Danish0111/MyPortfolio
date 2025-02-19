023add11802e5e42f06871219052ac9bf05ea2e8
Danish0111 <164894540+Danish0111@users.noreply.github.com>
2025-02-19 20:14:39 +0530

    env and contact url

--- my-project/src/Components/Contact.jsx
+++ my-project/src/Components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
             message: data.message
         }
         try{
-            const response = await fetch("http://localhost:5000", {
+            const response = await fetch("https://my-portfolio-backend-rust-delta.vercel.app", {
                 method: "POST", 
                 headers: {
                     "Content-Type": "application/json"
