78045aaab51da987ca865be81da2be5ca51fc464
Danish0111 <164894540+Danish0111@users.noreply.github.com>
2025-05-26 12:46:02 +0530

    skill section modified

--- my-project/src/Components/Skills.jsx
+++ my-project/src/Components/Skills.jsx
@@ -4,7 +4,7 @@ import { database } from '../firebase';
 import { ref, onValue } from "firebase/database";
 import ProgressBar from "@ramonak/react-progress-bar";
 import ProgressProvider from './ProgressProvider';
-import {motion, useScroll} from 'motion/react'
+import {motion, scale, useScroll} from 'motion/react'
 
 const Skills = () => {
   const db = database;
@@ -37,7 +37,7 @@ const Skills = () => {
           {/* <div className="line w-[110px] h-[1px] bg-white"></div> */}
         </div>
 
-        <div className='grid lg:grid-cols-2 gap-12 gap-x-0 py-2 mx-0 sm:mx-10 lg:mx-14'>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-12 gap-x-0 py-2 mx-0 sm:mx-10 lg:mx-16 justify-items-center'>
           {loading
             ? [...Array(skills.length || 7)].map((_, index) => (
               <div key={index} className="skill flex flex-col gap-2 justify-center items-center animate-pulse">
@@ -54,21 +54,25 @@ const Skills = () => {
               </div>
             ))
             : skills.map(skill => (
-              <motion.div initial={{scale: 0.5, y: -100, opacity: 0}} whileInView={{scale: 1, opacity: 1, y: 0}} transition={{duration: 1}} key={skill.id} className="skill flex justify-center items-center">
-                <div className='w-[90%] lg:w-[80%] flex justify-center gap-3'>
-                  <span className=''><img className='w-14 hover:scale-110 transition-all duration-[0.3s]' src={skill.imageURL} alt={skill.name} /></span>
-                  <ProgressProvider valueStart={0} valueEnd={skill.percentage}>
-                    {(value) => (
-                      <div className="flex flex-col w-[100%]">
-                        <div className="flex justify-between">
-                          <span>{skill.name}</span>
-                          <span>{skill.percentage}%</span>
-                        </div>
-                        <ProgressBar className='w-[100%] text-sm custom-progress' labelClassName="label" completed={value} height="10px" bgColor='var(--main-color)' baseBgColor='#646464' />
-                      </div>
-                    )}
-                  </ProgressProvider>
-                </div>
+              // <motion.div initial={{scale: 0.5, y: -100, opacity: 0}} whileInView={{scale: 1, opacity: 1, y: 0}} transition={{duration: 1}} key={skill.id} className="skill flex justify-center items-center">
+              //   <div className='w-[90%] lg:w-[80%] flex justify-center gap-3'>
+              //     <span className=''><img className='w-14 hover:scale-110 transition-all duration-[0.3s]' src={skill.imageURL} alt={skill.name} /></span>
+              //     <ProgressProvider valueStart={0} valueEnd={skill.percentage}>
+              //       {(value) => (
+              //         <div className="flex flex-col w-[100%]">
+              //           <div className="flex justify-between">
+              //             <span>{skill.name}</span>
+              //             <span>{skill.percentage}%</span>
+              //           </div>
+              //           <ProgressBar className='w-[100%] text-sm custom-progress' labelClassName="label" completed={value} height="10px" bgColor='var(--main-color)' baseBgColor='#646464' />
+              //         </div>
+              //       )}
+              //     </ProgressProvider>
+              //   </div>
+              // </motion.div>
+              <motion.div initial={{scale: 0.5, y: -100, opacity: 0}} whileHover={{scale: 1.14}} whileInView={{scale: 1, opacity: 1, y: 0}} transition={{duration: 1}} key={skill.id} className="skillCard w-32 sm:w-36 flex flex-col justify-center items-center gap-2 bg-[#121213ee] rounded-xl shadow-xl p-4">
+                <motion.img whileHover={{scale: 1.02}} src={skill.imageURL} alt="" />
+                <span className='font-bold'>{skill.name}</span>
               </motion.div>
             ))
           }
